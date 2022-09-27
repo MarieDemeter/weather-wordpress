26eacc4e9b57516fca4d7c1311a9bfce16816caa
MarieDemeter <mdemeter.ducrot@gmail.com>
2022-09-27 15:48:09 +0200

    refactor: :zap: REfacto plugin geolocator

--- wp-content/plugins/cnalps-geolocator/CNAlpsGeolocate.php
+++ wp-content/plugins/cnalps-geolocator/CNAlpsGeolocate.php
@@ -24,19 +24,20 @@ class Geolocator
         $latitude = $atts['latitude'];
         $longitude = $atts['longitude'];
         $zoom = $atts['zoom'];
+        $id = $atts['id'];
 
-        echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/leaflet.css">
+        return '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/leaflet.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/leaflet.js"></script>
         
-        <div id="cnalps-map" style="width: 100%; height: 300px;"></div>
+        <div id="'.$id.'" style="width: 300px; height: 300px;"></div>
         <script>
-            let map = L.map(\'cnalps-map\').setView([' . $latitude . ', ' . $longitude . '], '.$zoom.');
+            let map'.$id.' = L.map(\''.$id.'\').setView([' . $latitude . ', ' . $longitude . '], '.$zoom.');
         
             L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
                 attribution: \'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors\'
-            }).addTo(map);
+            }).addTo(map'.$id.');
         
-            L.marker(['.$latitude.','.$longitude.']).addTo(map)
+            L.marker(['.$latitude.','.$longitude.']).addTo(map'.$id.')
                 .bindPopup(\'La Tour de Crest\')
                 .openPopup();
         </script>';
