03a4f7d6de3454a62319cf1d1815a016e16651b2
MarieDemeter <mdemeter.ducrot@gmail.com>
2022-09-27 09:45:03 +0200

    feat: :zap: Add shortcode my_weather

--- wp-content/plugins/cnalps-weather/CNAlpsWeather.php
+++ wp-content/plugins/cnalps-weather/CNAlpsWeather.php
@@ -5,12 +5,26 @@
  */
 
 
-add_action("wp_footer", "print_Weather");
-
-
-function print_Weather()
+class CNAlpsWeather
 {
-    echo "<div class='cnalps-weather-widget'>
-            <div class='weather-title'>Météo à Crest</div>
-        </div>";
+    // déclaration d'une propriété
+
+
+
+    // déclaration des méthodes
+
+    public function __construct()
+    {
+        add_shortcode("my_weather", [$this, "print_weather"]);
+    }
+
+
+    function print_weather()
+    {
+        echo "<div class='cnalps-weather-widget'>
+                <div class='weather-title'>Météo à Crest</div>
+            </div>";
+    }
 }
+
+$cnalpsWeather = new CNAlpsWeather();
